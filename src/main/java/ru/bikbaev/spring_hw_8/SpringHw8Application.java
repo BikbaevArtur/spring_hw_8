12db75230f3b54720301ae2bc0c6dddfbd198117
arturbikbaev <bigbyar4i@icloud.com>
2024-06-03 21:36:13 +0500

    hw_8

---
+++ src/main/java/ru/bikbaev/spring_hw_8/SpringHw8Application.java
@@ -0,0 +1,13 @@
+package ru.bikbaev.spring_hw_8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHw8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHw8Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/bikbaev/spring_hw_8/SpringHw8ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bikbaev.spring_hw_8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHw8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
